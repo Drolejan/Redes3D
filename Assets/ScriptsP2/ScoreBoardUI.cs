1912c9fc865f19e2877ed423089006f4bd2fa678
Alejandro Medina <drolejan@gmail.com>
2025-09-30 17:46:55 -0600

    update30sep

---
+++ Assets/ScriptsP2/ScoreBoardUI.cs
@@ -0,0 +1,46 @@
+using System.Linq;
+using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreboardUI : NetworkBehaviour
+{
+    public static ScoreboardUI Instance;
+
+    public TextMeshProUGUI myScoreText;
+    public Transform listParent;
+    public GameObject rowPrefab;
+
+    void Awake() => Instance = this;
+
+    void OnEnable()  => InvokeRepeating(nameof(RefreshNow), 0.1f, 0.5f);
+    void OnDisable() => CancelInvoke(nameof(RefreshNow));
+
+    public void RefreshNow()
+    {
+        // 1) Mi score (local)
+        var local = NetworkClient.localPlayer;
+        if (local)
+        {
+            var myStats = local.GetComponent<PlayerThirdPerson>();
+            if (myStats && myScoreText)
+                myScoreText.text = $"Mi score: {myStats.score}";
+        }
+
+        // 2) Lista de todos
+        foreach (Transform c in listParent) Destroy(c.gameObject);
+
+        var all = FindObjectsOfType<PlayerThirdPerson>()
+                 .OrderByDescending(p => p.score)
+                 .ToList();
+
+        foreach (var p in all)
+        {
+            var row = Instantiate(rowPrefab, listParent);
+            var txt = row.GetComponentInChildren<Text>();
+            if (txt)
+                txt.text = $"Player {p.netId}: {p.score}";
+        }
+    }
+}
--- Assets/coin.cs
+++ Assets/coin.cs
@@ -5,13 +5,13 @@ public class coin : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider collision)
@@ -22,4 +22,20 @@ public class coin : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    /*
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (!collision.gameObject.CompareTag("Player")) return;
+
+        var player = collision.gameObject.GetComponent<PlayerThirdPerson>();
+        if (player == null) return;
+
+        // En vez de Destroy local, pedimos al player local que mande Command con la referencia
+        var myId = GetComponent<NetworkIdentity>();
+        if (myId != null)
+            player.OnCoinPickedWithRef(myId, 1);
+        else
+            player.sumarPuntos(); // fallback si no tiene NetworkIdentity
+    }
+    */
 }

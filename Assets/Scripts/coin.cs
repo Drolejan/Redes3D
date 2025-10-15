96a52ca62a3272991ff659c8e458d058b81664b1
Alejandro Medina <drolejan@gmail.com>
2025-10-14 20:05:37 -0600

    moneda

--- Assets/Scripts/coin.cs
+++ Assets/Scripts/coin.cs
@@ -2,18 +2,6 @@ using UnityEngine;
 
 public class coin : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -22,7 +10,6 @@ public class coin : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
 
     /*
     private void OnTriggerEnter(Collider collision)
